6d5b98b04d8774239b015819d40c9cfc40704bc1
ren0rys <119013589+ren0rys@users.noreply.github.com>
2024-05-31 22:57:31 -0600

    Update RecursoDigital.java
    comentarios para el javadoc de la clase RecursoDigital

--- Proyecto INGSOFTWARE/RecursoDigital.java
+++ Proyecto INGSOFTWARE/RecursoDigital.java
@@ -1,10 +1,18 @@
+/**Está clase permite crear objetos de tipo RecursoDigital y contiene los métodos necesarios para 
+asignar los valores correspondientes a los atributos de los objetos creados*/
+
 class RecursoDigital {
+/** @param id es un numero que funciona como identificador del recurso
+@param nombre guarda el norme del recurso
+@param tipo define el tipo del recurso
+@param contenido guarda el contenido del recurso
+@param categoria guarda la categoria del recurso*/
     private int id;
     private String nombre;
     private String tipo;
     private String contenido;
     private String categoria;
-
+/**Método constructor que recibe las cadenas y las asigna a los atributos correspondientes*/
     public RecursoDigital(int id, String nombre, String tipo, String contenido, String categoria) {
         this.id = id;
         this.nombre = nombre;
@@ -12,7 +20,7 @@ class RecursoDigital {
         this.contenido = contenido;
         this.categoria = categoria;
     }
-
+/**Método para obtener el nombre y las categorias de los objetos*/
     public String getNombre() {
         return nombre;
     }
