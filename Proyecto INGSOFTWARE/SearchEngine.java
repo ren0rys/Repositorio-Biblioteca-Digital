42dbb4318973f2de98e67bf53797683c8e7a893c
ren0rys <119013589+ren0rys@users.noreply.github.com>
2024-05-31 23:45:56 -0600

    Update SearchEngine.java
    comentarios javadoc

--- Proyecto INGSOFTWARE/SearchEngine.java
+++ Proyecto INGSOFTWARE/SearchEngine.java
@@ -1,12 +1,27 @@
+
 import java.util.ArrayList;
 import java.util.List;
+/**
+ * La clase SearchEngine representa un motor de búsqueda simple para recursos digitales.
+ * Permite buscar recursos por nombre y categoría.
+ */
 
 class SearchEngine {
     private List<RecursoDigital> recursos;
+/**
+     * Construye un SearchEngine con una lista de recursos digitales.
+     *
+     * @param recursos la lista de recursos digitales a ser buscados
+     */
 
     public SearchEngine(List<RecursoDigital> recursos) {
         this.recursos = recursos;
     }
+    /**
+     * Busca recursos digitales que contengan la consulta especificada en su nombre.
+     * @param query la cadena de consulta para buscar en los nombres de los recursos
+     * @return una lista de recursos digitales que contienen la consulta en su nombre
+     */
 
     public List<RecursoDigital> search(String query) {
         List<RecursoDigital> resultados = new ArrayList<>();
@@ -17,6 +32,12 @@ class SearchEngine {
         }
         return resultados;
     }
+/**
+     * Busca recursos digitales que pertenezcan a la categoría especificada.
+     *
+     * @param categoria la categoría para buscar en los recursos
+     * @return una lista de recursos digitales que pertenecen a la categoría especificada
+     */
 
     public List<RecursoDigital> searchByCategory(String categoria) {
         List<RecursoDigital> resultados = new ArrayList<>();
